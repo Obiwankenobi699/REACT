4dc0617d4932dcf8401e4c1f949efdaf9a3739ee
Mukul Mishra <mukulmishra795@gmail.com>
2025-08-12 11:25:09 +0530

    done ui and flow

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,11 +1,16 @@
+import { useContext } from "react";
+import {AiFillDelete} from "react-icons/ai"
+import { PostList } from "../store/post-list-store";
+
 const Post = ({ post }) => {
+  const {deletePost} = useContext(PostList)
   return (
     <div className="card post-card" style={{ width: "18rem" }}>
       <div className="card-body">
         <h5 className="card-title">
           {post.title}
           <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger ms-2">
-            {post.reactions}
+            <AiFillDelete onClick={() => deletePost(post.id)}/>
           </span>
         </h5>
 
@@ -19,6 +24,9 @@ const Post = ({ post }) => {
           ))}
         </div>
       </div>
+       <div className="alert alert-success p-2 py-1 reactions" role="alert">
+          👍 Reactions: {post.reactions}
+        </div>
     </div>
   );
 };
